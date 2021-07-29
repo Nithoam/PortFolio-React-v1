213b949a0457cb7b0d40751ea094f8db912656c5
Nithoam <nithoam.nicolas@gmail.com>
2021-07-30 01:34:13 +0200

    modification du CSS

--- src/components/Experiences/index.js
+++ src/components/Experiences/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './experiences.scss'
 
-var Carousel = require( '3d-react-carousal').Carousel;
+const Carousel = require( '3d-react-carousal').Carousel;
 
 const Experiences = () => {
-  console.log(Carousel)
   let slides = [
     <div className="experience">
       <h3 className="experience-title">[2019-2020] - Vendeur Adjoint Micromania</h3>
@@ -15,33 +14,37 @@ const Experiences = () => {
         <li>- Merchandising</li>
         <li>- Recrutements</li>
       </ul>
-    </div>,
+    </div>
+    ,
     <div className="experience">
-    <h3 className="experience-title">[2016-2019] - Vendeur/Préparateur Micromania</h3>
-    <p className="experience-subtitle">Magasin Micromania Défense 2 (magasin top 3 national)</p>
-    <ul className="experience-list">
-      <li>- Prise en charge de la clientèle</li>
-      <li>- Surveillance de la surface de vente</li>
-      <li>- Gestions des stocks</li>
-    </ul>
-  </div>  ,
+      <h3 className="experience-title">[2016-2019] - Vendeur/Préparateur Micromania</h3>
+      <p className="experience-subtitle">Magasin Micromania Défense 2 (magasin top 3 national)</p>
+      <ul className="experience-list">
+        <li>- Prise en charge de la clientèle</li>
+        <li>- Surveillance de la surface de vente</li>
+        <li>- Gestions des stocks</li>
+      </ul>
+    </div> 
+    ,
     <div className="experience">
-    <h3 className="experience-title">[2015-2016] - Employé polyvalent KFC</h3>
-    <p className="experience-subtitle">Cuisinier au KFC de Paris Alésia (Paris XIV)</p>
-    <ul className="experience-list">
-      <li>Production des différentes commandes lié à l'activité du restaurant</li>
-    </ul>
-  </div> , 
+      <h3 className="experience-title">[2015-2016] - Employé polyvalent KFC</h3>
+      <p className="experience-subtitle">Cuisinier au KFC de Paris Alésia (Paris XIV)</p>
+      <ul className="experience-list">
+        <li>Production des différentes commandes lié à l'activité du restaurant</li>
+      </ul>
+    </div> 
+    , 
     <div className="experience">
-    <h3 className="experience-title">[juil.2015 &#38; juil.2016] - Façonneur</h3>
-    <p className="experience-subtitle">E-center Malakoff - Remplacement saisonnier</p>
-    <ul className="experience-description">
-      <li>- Réalisation de livres photos</li>
-      <li>- Mise en cartons des commandes professionnelles</li>
-      <li>- Expédition des commandes personnelles</li>
-    </ul>
-    </div>  ,
-  ] ;
+      <h3 className="experience-title">[juil.2015 &#38; juil.2016] - Façonneur</h3>
+      <p className="experience-subtitle">E-center Malakoff - Remplacement saisonnier</p>
+      <ul className="experience-description">
+        <li>- Réalisation de livres photos</li>
+        <li>- Mise en cartons des commandes professionnelles</li>
+        <li>- Expédition des commandes personnelles</li>
+      </ul>
+    </div>
+    ,
+  ];
   return (
     <div className="experiences">
       <Carousel slides={slides} autoplay={false} interval={1000}/>
--- src/components/Formations/index.js
+++ src/components/Formations/index.js
@@ -6,9 +6,9 @@ import './formations.scss';
 const Formations = () => (
   <div className="formations main-component">
     <div className="formation">
-      <h2 className="formations-title">2021 - Formation Développeur web</h2>
-      <h3 className="formations-subtitle">O’clock, Labellisée Grand Ecole du Numérique</h3>
-      <div className="formations-description">
+      <h2 className="formation-title">2021 - Formation Développeur web</h2>
+      <h3 className="formation-subtitle">O’clock, Labellisée Grand Ecole du Numérique</h3>
+      <div className="formation-description">
         <ul>
           <li>- 3 mois de Socle : HTML/CSS/Javascript/PHP/MySQL</li>
           <li>- 1 mois de Spécialisation : React.js</li>
@@ -18,9 +18,9 @@ const Formations = () => (
     </div>
 
     <div className="formation">
-      <h2 className="formations-title">2014 - Baccalaureat Scientifique</h2>
-      <h3 className="formations-subtitle"> Lycée Sainte Géneviève – Paris VI </h3>
-      <div className="formations-description">
+      <h2 className="formation-title">2014 - Baccalaureat Scientifique</h2>
+      <h3 className="formation-subtitle"> Lycée Sainte Géneviève – Paris VI </h3>
+      <div className="formation-description">
         <p>Spécialité Science et Vie de la Terre</p>
       </div>
     </div>
