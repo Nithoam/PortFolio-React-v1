e4a1303bbc90823f2965ad652ee1414bcfbd4580
Nithoam <nithoam.nicolas@gmail.com>
2021-07-30 23:00:54 +0200

    mise en place des composants slide dans le carrousel

--- src/components/Experiences/index.js
+++ src/components/Experiences/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './experiences.scss'
+import './experiences.scss';
+import logoMM from './logoMM.png';
+import logoKFC from './logoKFC.png';
+import logoECenter from './logoEcenter.png';
 
 const Carousel = require( '3d-react-carousal').Carousel;
 
 const Experiences = () => {
   let slides = [
     <div className="experience">
-      <h3 className="experience-title">[2019-2020] - Vendeur Adjoint Micromania</h3>
+      {/* <img className="experience-logo" src={logoMaestra} alt="logo Maestra" /> */}
+      <div className="experience-time">[Juillet 2021]</div><h3 className="experience-title">Maestra</h3>
+      <p className="experience-subtitle">Projet de groupe de fin de formation : Création d'un site sur les pilules contraceptives</p>
+      <ul className="experience-list">
+        <li>- Developpement front du site, en React.js </li>
+        <li>- Rôle de scrum master</li>
+        <li>- Tenu du journal de bord du groupe</li>
+        <li>- Présentation orale, sur Youtube, du projet</li>
+      </ul>
+    </div>
+    ,
+    <div className="experience">
+      <img className="experience-logo" src={logoMM} alt="logo Micromania" />
+      <div className="experience-time">[2019-2020]</div><h3 className="experience-title">Vendeur Adjoint Micromania </h3>
       <p className="experience-subtitle">Accompagnement de mes supérieurs hiérarchiques, dans la gestion du magasin Micromania Beaugrenelle (top 20 national)</p>
       <ul className="experience-list">
         <li>- Réceptions des livraisons</li>
@@ -17,7 +33,8 @@ const Experiences = () => {
     </div>
     ,
     <div className="experience">
-      <h3 className="experience-title">[2016-2019] - Vendeur/Préparateur Micromania</h3>
+      <img className="experience-logo" src={logoMM} alt="logo Micromania" />
+      <div className="experience-time">[2016-2019]</div><h3 className="experience-title">Vendeur/Préparateur Micromania </h3>
       <p className="experience-subtitle">Magasin Micromania Défense 2 (magasin top 3 national)</p>
       <ul className="experience-list">
         <li>- Prise en charge de la clientèle</li>
@@ -27,17 +44,17 @@ const Experiences = () => {
     </div> 
     ,
     <div className="experience">
-      <h3 className="experience-title">[2015-2016] - Employé polyvalent KFC</h3>
-      <p className="experience-subtitle">Cuisinier au KFC de Paris Alésia (Paris XIV)</p>
-      <ul className="experience-list">
-        <li>Production des différentes commandes lié à l'activité du restaurant</li>
-      </ul>
+      <img className="experience-logo" src={logoKFC} alt="logo KFC" />
+      <div className="experience-time">[2015-2016]</div><h3 className="experience-title">Employé polyvalent KFC</h3>
+      <p className="experience-subtitle">Cuisinier KFC (Paris XIV)</p>
+      <p className="experience-description">Production des différentes commandes lié à l'activité du restaurant</p>
     </div> 
     , 
     <div className="experience">
-      <h3 className="experience-title">[juil.2015 &#38; juil.2016] - Façonneur</h3>
+      <img className="experience-logo" src={logoECenter} alt="logo E-Center" />
+      <div className="experience-time">[juil.2015 &#38; juil.2016]</div><h3 className="experience-title">Façonneur</h3>
       <p className="experience-subtitle">E-center Malakoff - Remplacement saisonnier</p>
-      <ul className="experience-description">
+      <ul className="experience-list">
         <li>- Réalisation de livres photos</li>
         <li>- Mise en cartons des commandes professionnelles</li>
         <li>- Expédition des commandes personnelles</li>
